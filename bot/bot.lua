5928971a022b8993f4ff7e785651a223fdbca4e1
RockySeven3161 <babaei.alireza580@GMAIL.COM>
2015-11-06 04:06:22 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {our_id},
+    sudo_users = {PhoneFixer1},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
